858eaad9c4321a7c81877c609d3efb074fc7dc9e
alexlhanson <alexlhanson@gmail.com>
2018-10-24 22:22:38 -0700

    add middleware for logging

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,4 +1,7 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from '../reducer/index';
+import logger from '../lib/middleware/logger';
 
-export default () => createStore(reducer);
+export default () => createStore(reducer, applyMiddleware(
+  logger,
+));
